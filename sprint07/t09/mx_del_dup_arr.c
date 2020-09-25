4a231f800a6dd0960adb2dc7bb506ac7c997107c
macbook <ilasapoznikov@MacBook-Air-Ila.local>
2020-09-25 10:40:16 +0300

    Fix

--- sprint07/t09/mx_del_dup_arr.c
+++ sprint07/t09/mx_del_dup_arr.c
@@ -12,6 +12,9 @@ int *mx_del_dup_arr(int *src, int src_size, int *dst_size) {
     }
     int dup_count = 0;
     for (int i = 0; i < src_size - 1; i++) {
+        if (i != 0 && src[i] == src[i - 1]) {
+            continue;
+        }
         for (int j = i + 1; j < src_size; j++) {
             if (src[i] == src[j]) {
                 dup_count++;
@@ -24,16 +27,15 @@ int *mx_del_dup_arr(int *src, int src_size, int *dst_size) {
     bool is_dup;
     for (int i = src_size - 1; i >= 0; i--) {
         is_dup = false;
-        for (int j = i - 1; j >=  0; j--) {
+        for (int j = i - 1; j >= 0; j--) {
             if (src[i] == src[j]) {
                 is_dup = true;
             }
         }
-        if (!is_dup && d >=0) {
+        if (!is_dup && d >= 0) {
             result[d] = src[i];
             d--;
         }
     }
     return result;
 }
-
