fb918e214d8a9db73a4756b472b41ed8659ce596
macbook <ilasapoznikov@MacBook-Air-Ila.local>
2020-09-25 18:52:06 +0300

    Update

---
+++ sprint08/t05/choice.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef enum e_phrase 
+{
+    MX_RED_PILL, 
+    MX_BLEU_PILL
+}       t_phrase;
+
+char *MX_SUCCESS_PHRASE = "Follow me!";
+char *MX_FAIL_PHRASE = "Perhaps I was wrong about you, Neo.";
+char *MX_UNDEFINED_PHRASE = "Are you sure about that?";
